8540e927dc7c0280e6b85ddf876a140d5eafd3a0
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-03-21 16:31:20 +0700

    Commit 5 menambahkan

--- file1.cpp
+++ file1.cpp
@@ -45,4 +45,14 @@ void insertionSort()
     
     j = i - 1; // Step 3
     
-   
+    while (j >= 0 && arr[j] > temp) // Step 4
+    {
+    arr[j + 1 ] = arr[j]; //Step 4a
+    j--; // Step 4b
+    }
+    
+    arr[j + 1] = temp; // Step 5
+    } 
+    }
+    
+    
