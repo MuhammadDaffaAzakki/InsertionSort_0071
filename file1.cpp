37bd7e0fffac279cff52aad42dd173358338d785
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-03-21 16:29:20 +0700

    Commit 3 Menambahkan

--- file1.cpp
+++ file1.cpp
@@ -25,3 +25,13 @@ cout << "======================" << endl;
 cout << "Masukkan Element Array" << endl;
 cout << "======================" << endl;
 
+for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+{
+cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginput nilai data n
+cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
+}
+}
+
+void insertionSort()
+{ // Procedure InsertionSort
+
