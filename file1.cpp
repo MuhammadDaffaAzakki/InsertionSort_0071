35cf875fd8c0c48f74fe6470c51d2b3795417cfc
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-03-21 16:26:30 +0700

    Commit 1 membuat array

---
+++ file1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int arr[20]; // Membuat Array dengan panjang data 20
+int n; // Membuat Variable inputan n
+
+void input()
+{ // Produce Input
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+cin >> n; // memanggil variable inputan n
+
